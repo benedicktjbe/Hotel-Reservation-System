6e1d5681c0965067d9733d82a20636b05173d934
sarge-pato <ujinyokogan2@gmail.com>
2025-11-16 00:09:19 +0800

    Added a loop for each invalid input from the user

--- src/HotelReservationSystem.java
+++ src/HotelReservationSystem.java
@@ -4,6 +4,9 @@ public class HotelReservationSystem {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        char choice;
+
+        do {
         System.out.println("-----------------------------------------");
         System.out.println("Welcome to the Hotel Reservation System!");
         System.out.println("Please choose an option: ");
@@ -13,8 +16,12 @@ public class HotelReservationSystem {
         System.out.println("D. Check-out Guest / Generate bill");
         System.out.println("E. Exit");
         System.out.print("Enter your choice here: ");
-        char choice = sc.nextLine().toUpperCase().charAt(0);
+        choice = sc.nextLine().toUpperCase().charAt(0);
         System.out.println("-----------------------------------------");
+        if (choice != 'A' && choice != 'B' && choice != 'C' && choice != 'D' && choice != 'E') {
+                System.out.println("Invalid choice. Please try again.");
+            }
+        } while (choice != 'A' && choice != 'B' && choice != 'C' && choice != 'D' && choice != 'E');
 
         switch (choice) {
             case 'A' -> {
