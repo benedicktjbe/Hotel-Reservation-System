50575035ce3a6629c4a90b947f0d00fcd4ec66cf
Joshua <jbe2262492@slu.edu.ph>
2025-11-15 22:47:34 +0800

    Added main template

---
+++ src/HotelReservationSystem.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class HotelReservationSystem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("-----------------------------------------");
+        System.out.println("Welcome to the Hotel Reservation System!");
+        System.out.println("Please choose an option: ");
+        System.out.println("A. Check Room Availability");
+        System.out.println("B. Make New Reservation");
+        System.out.println("C. Check-in Guest");
+        System.out.println("D. Check-out Guest / Generate bill");
+        System.out.println("E. Exit");
+        System.out.print("Enter your choice here: ");
+        char choice = sc.nextLine().toUpperCase().charAt(0);
+        System.out.println("-----------------------------------------");
+
+        switch (choice) {
+            case 'A' -> {
+                //Check Room Availability Method
+            }
+            case 'B' -> {
+                //Make New Reservation Method
+            }
+            case 'C' -> {
+                //Check-in Guest Method
+            }
+            case 'D' -> {
+                //Check-out Guest Method
+            }
+            case 'E' -> {
+                System.out.println("Thank you for using our Hotel Reservatiuon System!");
+                System.exit(0);
+            }
+        }
+    }
+}
