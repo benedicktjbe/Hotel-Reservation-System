40da28c2517aea2fb7917844068bd5ce16aebde6
Joshua <jbe2262492@slu.edu.ph>
2025-12-03 03:26:35 +0800

    FINAL FINAL

--- src/FinalProjectGroup6.java
+++ src/FinalProjectGroup6.java
@@ -80,7 +80,7 @@ Algorithm: Hotel Reservation System
        chooses the exit option.
 */
 
-public class HotelReservationSystem {
+public class FinalGroupProjectGroup6 {
     static String[][] standardRoom = {
             { "     ", "Day 1","Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7", "Day 8", "Day 9", "Day 10" },
             { "S101", "Free","Free", "Free", "Free", "Free", "Free", "Free", "Free", "Free", "Free" },
